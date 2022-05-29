e776217e95bf7d2485d5fd091771b50ddf744ed8
ummazannathrishad <83083158+ummazannathrishad@users.noreply.github.com>
2022-05-29 15:21:49 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './vendor/css/style.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    
+    <>
+    <Footer />
+    </>
   );
 }
 
