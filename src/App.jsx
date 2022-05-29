0690a10cb38d84789ad9290565abd4bb546754f4
Tanmoy Barua <43274420+Tanmoy-Barua@users.noreply.github.com>
2022-05-29 16:09:03 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './vendor/css/style.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ContactUs from './components/contact/contactUs';
+import Singlepage from './components/singlepage/singlepage';
 
 function App() {
   return (
     <>
-    <Header />
-    <Footer />
-    </>
+    {/* <Login /> */}
+    {/* Route */}
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Contact-Us" element={<ContactUs />} />
+        <Route path="/news-details" element={<Singlepage />} />
+      </Routes>
+    </BrowserRouter>
+  </>
   );
 }
 
